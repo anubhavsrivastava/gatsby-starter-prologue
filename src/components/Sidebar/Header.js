cf78ff8633a4d9b45637b2920dc73f592562905b
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-18 18:58:17 +0530

    fixed header link

--- src/components/Sidebar/Header.js
+++ src/components/Sidebar/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default function Footer({ title, heading, avatar }) {
   return (
     <div id="logo">
-      <span className="image avatar48">
-        <img height="128px" src={avatar} alt="" />
-      </span>
-      <a href="/#">
+      <Link to="/">
+        <span className="image avatar48">
+          <img height="128px" src={avatar} alt="" />
+        </span>
+
         <h1 id="title">{title}</h1>
-      </a>
-      <p>{heading}</p>
+        <p>{heading}</p>
+      </Link>
     </div>
   );
 }
